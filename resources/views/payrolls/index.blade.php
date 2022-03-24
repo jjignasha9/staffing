13af73efb5fefba665d688a400414ddf3c125bc7
Shaloni <dl.shalonitailor>
2022-03-24 12:27:13 -0400

    changes

--- resources/views/payrolls/index.blade.php
+++ resources/views/payrolls/index.blade.php
@@ -44,43 +44,6 @@
 					</div>
 				</div>	
 			</div>	
-			@foreach($timesheets as $workdays)
-			<div class="bg-white mt-3 rounded-xl p-5">
-				<div class="items-center">
-					<div class="grid grid-cols-12">
-						
-						<div class="col-span-6">
-							<div class="flex items-center gap-10">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
-									<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-								</svg>
-
-								<div class="text-sm font-bold">
-									{{ $workdays[0]->employee_name }}
-								</div>
-							</div>
-						</div>
-
-						
-						<div class="col-span-6">
-							@foreach($workdays as $workday)
-							<div class="grid grid-cols-12 tracking-wide text-sm">
-								<div class="col-span-4">{{ $workday->shift_name }}</div>
-								<div class="col-span-3">$ {{ $workday->pay_rate }}</div>	
-								<div class="col-span-2">{{ $workday->total_hours }} hrs</div>
-								<div class="col-span-3 text-center">$ {{ $workday->total_amount }}</div>
-							</div>
-							@endforeach
-
-							<div class="grid grid-cols-8 mt-5">
-                    	 		<div class="text-sm font-bold col-start-5 col-span-2 ml-10">{{ $total = $workdays->sum('total_hours') }} hrs</div>
-                    	 		<div class="text-sm font-bold col-start-7 col-span-2 ml-8">$ {{ $amount = $workdays->sum('total_amount') }}</div>
-                    	 	</div>
-
-						</div>
-					</div>
-				</div>
-			</div>
 
 			@foreach($timesheets as $workdays)
 					<div class="bg-white mt-3 rounded-xl p-5">
