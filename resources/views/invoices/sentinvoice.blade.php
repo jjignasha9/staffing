cacc3b76308541c028bc88c9269f11303995847d
Janvi <dl.janvikabariya@gmail.com>
2022-04-04 16:18:56 -0400

    resend mail

--- resources/views/invoices/sentinvoice.blade.php
+++ resources/views/invoices/sentinvoice.blade.php
@@ -25,15 +25,11 @@
 	    </div>      
 
 	    <div class="col-span-10">
-		<div class="flex justify-between items-center">
-			<div class="flex">
 				@foreach($day_weekends as $day_weekend)
-				<div class="bg-white p-1 rounded-full w-32 text-center mr-10 shadow {{ $day_weekend == $active_day_weekend ? 'bg-teal-700 text-white' : '' }}">
-					<span class="text-sm">{{ $day_weekend }}</span>
-				</div>
+				<a href="{{ route('invoices.sent-invoice',$day_weekend) }}" class="bg-white rounded-full py-1 px-4 text-sm mx-2 {{ $day_weekend == $active_day_weekend ? 'bg-teal-700 text-white' : '' }}">{{ Carbon\carbon::parse($day_weekend)->format('m/d/Y') }}
+				</a>
 				@endforeach
-			</div>
-		</div>    
+		   
 
 		<div class="bg-white mt-5 rounded-full px-5 py-1.5">
 			<div class="grid grid-cols-12">
@@ -51,7 +47,7 @@
 			<div class="grid grid-cols-12">
 				<div class="text-sm text-slate-500 col-span-5">
 					<div class="text-black font-semibold">{{ $sentdata_invoice->client_name }}</div>
-					<div class="text-sm">{{ $sentdata_invoice->id }}</div>
+					<div class="text-sm"># {{ $sentdata_invoice->id }}</div>
 					<div class="text-sm">{{ $sentdata_invoice->employee_name }}</div>
 				</div>
 
@@ -63,8 +59,8 @@
 				</div>
 
 				<div class="text-sm text-slate-400 col-span-2 hover:text-teal-600">
-					<button class="flex border border-teal-600 w-32 rounded-full font-bold p-2">
-						<div><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
+					<button class="flex border border-teal-600 w-30 rounded-full font-bold py-1 px-2">
+						<div><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 							  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
 							</svg></div>
 						<div class="ml-2">{{ $sentdata_invoice->status_name }}</div>
@@ -93,7 +89,7 @@
 				</div>
 
 				<div class="text-sm text-slate-500 col-span-4">
-					<button class="bg-teal-600 p-2 text-white rounded-full w-48 ml-10 hover:bg-teal-700">Resend invoices</button>
+					<a href="{{ route('invoices.resend-invoice',$active_day_weekend) }}" class="bg-teal-600 p-2 text-white rounded-full w-48 ml-10 hover:bg-teal-700">Resend invoices</a>
 				</div>
 			</div>
 		</div>
