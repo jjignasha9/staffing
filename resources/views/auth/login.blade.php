534d195037d73c04d902f12c460325cd3958c713
Jignasha <dl.jignashajethava@gmail.com>
2022-04-06 11:30:51 +0530

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title></title>
 </head>
-<body>
+<body class="bg-cover" style="background-image: url('https://cdn.wallpapersafari.com/13/73/AQ4CSR.jpg');">
     <div class="px-72 py-36">
         <div class="grid grid-cols-12">
             <div class="col-span-3">
@@ -16,10 +16,10 @@
                 <center>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-                        <div class="col-span-4 bg-slate-100 mt-5 border border-cyan-700 p-10 bg-slate-100 rounded">
-                            <h1 class="text-3xl text-blue-600 p-4 text-center font-bold font-serif tracking-wide">TimeSheet  </h1>
+                        <div class="col-span-4 mt-5 border border-teal-700 p-10 bg-teal-50 rounded">
+                            <h1 class="text-3xl text-teal-600 p-4 text-center font-bold font-serif tracking-wide">TimeSheet</h1>
                             <div>
-                                <input id="email" type="email" class="h-6 w-full p-5 bg-blue-100 rounded mb-2 border focus:border-blue-400 outline-none @error('email') is-invalid @enderror" value="{{ old('email') }}"  name="email" required autocomplete="email" autofocus>
+                                <input id="email" type="email" placeholder="Username" class="h-6 w-full p-5 rounded mb-2 border focus:border-teal-600 outline-none @error('email') is-invalid @enderror" value="{{ old('email') }}"  name="email" required autocomplete="email" autofocus>
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
                             </div>
 
                             <div>
-                                <input id="password" type="password" class="h-6 w-full p-5 bg-blue-100  rounded border focus:border-blue-400 outline-none @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" value="{{ old('password') }}">
+                                <input id="password" type="password" placeholder="Password" class="h-6 w-full p-5 rounded border focus:border-teal-600 outline-none @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" value="{{ old('password') }}">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -42,7 +42,7 @@
                                 <input type="checkbox" name="checkbox" class="mx-2" id="remember" {{ old('remember') ? 'checked' : '' }}>Remember Me
                             </div>
 
-                            <button type ="submit" class="w-full flex bg-blue-500 hover:bg-blue-700 p-3 text-white justify-center items-center rounded"> 
+                            <button type ="submit" class="w-full flex bg-teal-600 hover:bg-teal-700 p-3 text-white justify-center items-center rounded"> 
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white mr-2" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                                 </svg>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,8 +6,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title></title>
 </head>
-<body>
-    <div class="px-72 py-36">
+<body class="bg-cover" style="background-image: url('https://cdn.wallpapersafari.com/13/73/AQ4CSR.jpg');">
+    <div class="px-32 py-32">
         <div class="grid grid-cols-12">
             <div class="col-span-4">
 
@@ -17,11 +17,11 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="col-span-4 bg-slate-100 mt-5 border border-cyan-700 p-10 bg-slate-100 rounded">
-                            <h1 class="text-3xl text-blue-500 p-4 text-center font-bold font-serif tracking-wide">Register</h1>
+                        <div class="col-span-4 mt-5 border border-teal-700 p-10 bg-teal-50 rounded">
+                            <h1 class="text-3xl text-teal-600 p-4 text-center font-bold font-serif tracking-wide">Register</h1>
 
                             <div>
-                                <input id="name" type="text" placeholder="Name" class="h-6 w-full p-5 bg-blue-100 rounded mb-2 border focus:border-blue-400 outline-none @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" placeholder="Name" class="h-6 w-full p-5 rounded mb-2 border focus:border-teal-600 outline-none @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                  @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -31,7 +31,7 @@
                             </div>
 
                             <div> 
-                                <input id="email" type="email" placeholder="Email" class="h-6 w-full p-5 bg-blue-100 rounded mb-2 border focus:border-blue-400 outline-none @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" placeholder="Email" class="h-6 w-full p-5 rounded mb-2 border focus:border-teal-600 outline-none @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -42,7 +42,7 @@
 
 
                             <div>
-                                <input id="password" type="password" placeholder="Password" class="h-6 w-full p-5 bg-blue-100 rounded mb-2 border focus:border-blue-400 outline-none @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}"required autocomplete="new-password">
+                                <input id="password" type="password" placeholder="Password" class="h-6 w-full p-5 rounded mb-2 border focus:border-teal-600 outline-none @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}"required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -52,10 +52,10 @@
                             </div>
 
                              <div>
-                                <input id="password-confirm" type="password" placeholder="Confirm password" class="h-6 w-full p-5 bg-blue-100 rounded mb-2 border focus:border-blue-400 outline-none" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" placeholder="Confirm password" class="h-6 w-full p-5 rounded mb-2 border focus:border-teal-600 outline-none" name="password_confirmation" required autocomplete="new-password">
                             </div>
 
-                             <button type ="submit" class="w-full flex bg-blue-500 hover:bg-blue-700 p-3 text-white justify-center items-center rounded">       
+                             <button type ="submit" class="w-full flex bg-teal-600 hover:bg-teal-700 p-3 text-white justify-center items-center rounded">       
                                    Register
                             </button>
 
