123f5b230ce82a3e6dcb7947fb8cf33e391eb309
Shaloni <dl.shalonitailor>
2022-04-11 14:43:58 -0400

    chat

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -221,6 +221,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
             </svg>
          </button> 
+
+         <div class="chatmessege fixed right-4 bottom-14"></div>
+         
         <div id="chatbox" class="hidden fixed right-0 bottom-24 w-80 text-base max-h-screen  list-none bg-white rounded-lg shadow dark:bg-gray-700 mr-2" >
             <div class="flex justify-between items-center">
                 <div class="flex items-center p-3 text-gray-600">
@@ -430,6 +433,48 @@
                 });
           }
         }
+
+         if($('chatbox').hide()) {
+
+            var url = "/chats"; 
+            let count = 0;
+
+           $.ajax({
+                method:"GET",
+                url: url,
+               
+            }).done(function(data) {
+
+            
+                let html = '';
+               
+                var users = data.users;
+                var indicateUsers = data.indicate_users;
+
+                Object.keys(users).forEach(key => {
+
+                    if (indicateUsers.includes(users[key]['id'])) {
+
+                        count++;  
+                         playAudio();
+                        console.log(count);
+                    }
+
+                });
+
+                if(count != 0) {
+                    html += '<span class="bg-teal-500 rounded-full py-1 px-2 text-white indicator-'+ users['id'] +'">'+ count +'</span>';
+                     
+                }               
+
+                $('.chatmessege').html(html);
+
+                
+            }); 
+
+       
+        }
+
 
         $('.chatbutton').click(function() {
 
